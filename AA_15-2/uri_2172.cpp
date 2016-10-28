87d9f55c042a128bb93501a7b4e1dc1172f07b87
clarammdantas <maria.dantas@ccc.ufcg.edu.br>
2016-10-28 17:57:44 -0300

    Novas Questões URI/Codeforces

---
+++ AA_15-2/uri_2172.cpp
@@ -0,0 +1,19 @@
+// Evento - URI 2172
+// Clara Moraes Dantas
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	while (true) {
+		long long int x, m;
+		cin >> x >> m;
+
+		if (x == 0 && m == 0) break;
+
+		cout << x * m << endl;
+	}
+
+	return 0;
+}
